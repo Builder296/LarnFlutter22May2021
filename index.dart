0c953b311d0fc8f86f647096695e1077f13b30e1
builder296 <kessirin296@gmail.com>
2021-05-22 11:45:11 +0700

    Initail and login screen

---
+++ index.dart
@@ -0,0 +1,17 @@
+// void main() {
+//   String name = "Builder";
+//   Map<String, dynamic> address = {
+//     "homeNo": "555/555",
+//     "postCpde": 11130,
+//   };
+//   print(name);
+//   print(address["homeNo"]);
+
+//   int? age;
+//   age = 26;
+//   print(age);
+
+//   late int age2;
+//   age2 = 55;
+//   print(age2);
+// }
